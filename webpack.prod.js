eb1a3fe3c0e28b2b8ddf0e7a4f39832268acb518
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-28 23:01:38 +0700

    chore: install and setup cleanwebpackplugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,22 +1,25 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // installed via npm
+const { merge } = require("webpack-merge");
+
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: '/node_modules/',
+        exclude: "/node_modules/",
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              presets: ['@babel/preset-env'],
+              presets: ["@babel/preset-env"],
             },
           },
         ],
       },
     ],
   },
+  plugins: [new CleanWebpackPlugin()],
 });
