670fce198f157e842ac4d2fed8dfff4cce1d246a
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-28 22:51:40 +0700

    improvement: scroll smooth to main content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { html, render, TemplateResult } from "lit-html";
 import "@/app/shared/components/app-bar";
 import "@/app/shared/components/footer";
 
+function goToMainContent(e: Event): void {
+  e.preventDefault();
+  const element = document.querySelector("home-page");
+  const mainContent = element?.shadowRoot?.querySelector("#MainContent");
+
+  mainContent?.scrollIntoView({
+    behavior: "smooth",
+  });
+}
+
 const AppComponent: TemplateResult = html`
-  <a href="#MainContent" class="SkipLink">Menuju ke konten</a>
+  <a href="#" class="SkipLink" @click=${goToMainContent}>Menuju ke konten</a>
 
   <x-app-bar></x-app-bar>
 
