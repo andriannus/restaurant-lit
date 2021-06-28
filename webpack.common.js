eceeefd477b9cdf3e9c913c26cdba25bd1b1cfab
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-28 22:31:34 +0700

    chore: setup loader for file

--- webpack.common.js
+++ webpack.common.js
@@ -42,6 +42,14 @@ module.exports = {
         ],
       },
       {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
