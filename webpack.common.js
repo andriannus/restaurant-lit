18130fe65cf4f1cdd659de936202a6229631fa92
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-06-28 22:54:37 +0700

    chore: add favicon

--- webpack.common.js
+++ webpack.common.js
@@ -59,6 +59,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "public/index.html"),
+      favicon: path.resolve(__dirname, "public/favicon.ico"),
       filename: "index.html",
     }),
   ],
