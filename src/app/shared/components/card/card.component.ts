530c9e78befd03413b4ccdb79a8ab0a9f900fcb0
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-07-08 21:13:45 +0700

    improvement: adjust card component

--- src/app/shared/components/card/card.component.ts
+++ src/app/shared/components/card/card.component.ts
@@ -1,17 +1,51 @@
 import { CSSResultGroup, html, LitElement, TemplateResult } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property } from "lit/decorators.js";
+import { classMap } from "lit/directives/class-map";
 
 import { cardStyles } from "./card.style";
 
 @customElement("x-card")
 export default class CardComponent extends LitElement {
+  @property({ type: Boolean, reflect: true })
+  private hover: boolean;
+
+  @property({ type: Boolean, reflect: true })
+  private link: boolean;
+
+  @property({ type: String, reflect: true })
+  private to: string;
+
+  constructor() {
+    super();
+    this.hover = false;
+    this.link = false;
+    this.to = "";
+  }
+
   static get styles(): CSSResultGroup {
     return cardStyles;
   }
 
   render(): TemplateResult {
+    if (this.link) {
+      return html`
+        <a
+          class=${classMap({ Card: true, "is-hover": this.hover })}
+          href=${this.to}
+        >
+          <div class="Card-media">
+            <slot name="media"></slot>
+          </div>
+
+          <div class="Card-text">
+            <slot name="text"></slot>
+          </div>
+        </a>
+      `;
+    }
+
     return html`
-      <div class="Card">
+      <div class=${classMap({ Card: true, "is-hover": this.hover })}>
         <div class="Card-media">
           <slot name="media"></slot>
         </div>
--- src/app/shared/components/card/card.style.ts
+++ src/app/shared/components/card/card.style.ts
@@ -1,23 +1,38 @@
 import { css } from "lit";
 
 export const cardStyles = css`
+  a {
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: none;
+  }
+
+  a:focus {
+    outline: none;
+  }
+
   .Card {
     background: #ffffff;
     border-radius: 4px;
     box-shadow: 0 3px 1px -2px rgba(0, 0, 0, 0.2),
       0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12);
-    cursor: pointer;
+    cursor: default;
+    display: block;
     transition: all 200ms ease-in-out;
     max-width: 100%;
   }
 
-  .Card:focus,
-  .Card:hover {
+  .Card.is-hover:focus,
+  .Card.is-hover:hover {
     box-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
       0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
+    cursor: pointer;
   }
 
   .Card-text {
+    color: #000000;
     padding: 16px;
     text-align: justify;
   }
