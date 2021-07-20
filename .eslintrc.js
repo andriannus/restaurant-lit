f80fa3e1fbb9ea1af0bb0975925076d48837c1b4
Andriannus Parasian <andriannus.parasian@qoala.id>
2021-07-20 22:40:01 +0700

    chore: adjust eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,20 +3,18 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jasmine: true,
+    mocha: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:jasmine/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  plugins: ["@typescript-eslint", "jasmine"],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
   },
   rules: {
+    "@typescript-eslint/no-explicit-any": 0,
+    "@typescript-eslint/no-non-null-assertion": 0,
     "@typescript-eslint/no-var-requires": 0,
   },
 };
